918f4d4cee761018a7c08e0695761483ca00a6b4
nagaraj <nyk6426@gmail.com>
2026-05-24 16:24:39 +0530

    v1

---
+++ api-testing.py
@@ -0,0 +1,24 @@
+# Input two integers
+a = int(input("Enter first integer: "))
+b = int(input("Enter second integer: "))
+
+# Addition
+addition = a + b
+
+# Subtraction
+subtraction = a - b
+
+# Multiplication
+multiplication = a * b
+
+# Division
+if b != 0:
+    division = a / b
+else:
+    division = "Cannot divide by zero"
+
+# Display results
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
